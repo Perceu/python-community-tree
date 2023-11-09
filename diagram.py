addbbb5729b51f81f67980ab5aae41167fdf4aac
Perceu Bertoletti <perceubertoletti@gmail.com>
2023-11-09 10:09:54 -0300

    adicionado grupos na arvore de eventos

--- diagram.py
+++ diagram.py
@@ -13,17 +13,57 @@ with Diagram("Python Comunity", show=False, direction="TB"):
 
         with Cluster('Eventos Sul'): 
             [
+                Python("GruPy-PR"),
+                Python("Pug-SC"),
+                Python("GruPy Blumenau"),
+                Python("Python Floripa"),
                 Python("PyCaxias"),
                 Python("PyTchê")
             ] >> pythonSul
 
-        # with Cluster('Eventos Sudeste'): 
+        with Cluster('Eventos Sudeste'): 
+            [
+                Python("PythonOnRio"),
+                Python("GruPy-ES"),
+                Python("GruPy-Campinas"),
+                Python("GruPy-ABC"),
+                Python("GrupyBauru"),
+                Python("GruPy-SP"),
+                Python("Python Sorocaba"),
+                Python("grupy-sanca"),
+                Python("GruPy Sul Fluminense"),
+                Python("GruPy-RP"),
+                Python("Py013"),
+                Python("Pug-MG")
+            ] >> pythonSudeste
 
-        # with Cluster('Eventos Centro'): 
+        with Cluster('Eventos Centro'): 
+            [
+                Python("GruPy-GO"),
+                Python("GruPy-DF"),
+                Python("Pydata BSB"),
+                Python("GruPy-MT")
+            ] >> pythonCentro
 
-        # with Cluster('Eventos Nordeste'): 
+        with Cluster('Eventos Nordeste'): 
+            [
+                Python("Pug-SE"),
+                Python("GruPy-RN"),
+                Python("GruPy-BA"),
+                Python("Pug-CE"),
+                Python("GruPy-Al"),
+                Python("Pug-PI"),
+                Python("Pug-PE"),
+                Python("PUG-PB"),
+                Python("Pug-MA"),
+            ] >> pythonNordeste
 
-        # with Cluster('Eventos Norte'): 
+        with Cluster('Eventos Norte'): 
+            [
+                Python("PyNorte"),
+                Python("Pug-Am"),
+                Python("GruPy-RO")
+            ] >> pythonNorte
 
         [
             pythonSul, 
